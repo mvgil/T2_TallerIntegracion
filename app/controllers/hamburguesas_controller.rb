cbf8fdaf90c24e764d9f6124164561406732a13c
VIctoria GIl <mvgil@uc.cl>
2020-04-27 23:45:26 -0400

    skipauthenticity

--- app/controllers/hamburguesas_controller.rb
+++ app/controllers/hamburguesas_controller.rb
@@ -1,5 +1,6 @@
 class HamburguesasController < ApplicationController
   before_action :set_hamburguesa, only: [:show, :edit, :update, :destroy]
+  skip_before_action :verify_authenticity_token
 
   # GET /hamburguesas
   # GET /hamburguesas.json

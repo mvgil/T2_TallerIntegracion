4e8d9f5987b3815e8bdbbfab6fdfe6260ac6cf4f
VIctoria GIl <mvgil@uc.cl>
2020-04-28 00:17:07 -0400

    renderjson

--- app/controllers/hamburguesas_controller.rb
+++ app/controllers/hamburguesas_controller.rb
@@ -29,7 +29,7 @@ class HamburguesasController < ApplicationController
   # POST /hamburguesas.json
   def create
     @hamburguesa = Hamburguesa.new(hamburguesa_params)
-    render json: hamburguesa_params
+    render json: {"hola": "veamos"}
 
     respond_to do |format|
       if @hamburguesa.save
